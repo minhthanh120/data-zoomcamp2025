59409734f6d2d5127d703fcef106774036bbbbc8
DESKTOP-UHNFS2P\MinhThanh <minhthanh1200@hotmail.com>
2025-03-31 23:16:36 +0700

    Revert "Add files via upload"
    This reverts commit adc565ecd49a69b3a379f87a9655db8211ec93e8.

--- models/staging/report.sql
+++
@@ -1,4 +0,0 @@
-SELECT SUM(amount) as "volume of transactions", type
-FROM public.financial_transactions
-GROUP BY type
-
--- target/run/de_project/models/staging/report.sql
+++
@@ -1,9 +0,0 @@
-
-  create view "warehouse"."public"."report__dbt_tmp"
-    
-    
-  as (
-    SELECT SUM(amount) as "volume of transactions", type
-FROM public.financial_transactions
-GROUP BY type
-  );
